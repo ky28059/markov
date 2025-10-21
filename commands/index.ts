671f0e48129cc4067a939bdf31366505ef6f3248
Kevin Yu <kevinyu567@gmail.com>
2025-10-21 14:15:48 -0400

    Fix command type error

--- commands/index.ts
+++ commands/index.ts
@@ -1,5 +1,7 @@
+import type { Command } from '../util/commands';
+
 import fo from './fo';
 import so from './so';
 import weights from './weights';
 
-export default [fo, so, weights];
+export default [fo, so, weights] as Command[];
